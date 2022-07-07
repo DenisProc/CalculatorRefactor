96337017c38a7df09d592d9c1a8acbf0119af4d9
denis <denisdinoden@gmail.com>
2022-07-07 15:32:09 +0600

    calculator 2.1

--- app/src/main/java/com/example/calculatorrefactor/model/Theme.java
+++ app/src/main/java/com/example/calculatorrefactor/model/Theme.java
@@ -6,8 +6,8 @@ import androidx.annotation.StyleRes;
 import com.example.calculatorrefactor.R;
 
 public enum Theme {
-    DARK(R.style.Theme_CalculatorRefactorDark,R.string.key_darkTheme,"theme_1"),
-    LIGHT(R.style.Theme_CalculatorRefactorLight,R.string.key_lightTheme,"theme_2");
+    DARK(R.style.Theme_CalculatorRefactorDark, R.string.key_darkTheme, "theme_1"),
+    LIGHT(R.style.Theme_CalculatorRefactorLight, R.string.key_lightTheme, "theme_2");
 
     @StyleRes
     private final int themeRes;
